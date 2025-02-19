808caff31043eb5a578c02f4268529711ab8fb51
Lokesh <147271484+lokesh2910v@users.noreply.github.com>
2025-02-19 17:56:00 +0530

    Time: 1 ms (100%), Space: 42.3 MB (79.97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -2,9 +2,7 @@ class Solution {
     public int myAtoi(String s) {
 
         s=s.trim();
-        if(s.length()==0){
-            return 0;
-        }
+   if (s.isEmpty()) return 0;
         int i=0;
         int sign=1;
         if(s.charAt(i)=='-'){
